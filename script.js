ac6d361e1274a3af1863c11d1ba7e4abf9de34ad
improve777 <improve777@gmail.com>
2021-02-06 12:43:53 +0900

    크롬 플러그인 생성

---
+++ script.js
@@ -0,0 +1,16 @@
+// 티스토리 에디터에 있는 모든 텍스트의 길이를 가져온다
+
+// 크롬 확장의 기능에서 tabs 와 관련된 기능
+// 컨텐트 페이지를 대상으로 아래와 같은 코드를 실행한다
+chrome.tabs.executeScript(
+  {
+    code: 'var iframeId = document.getElementsByTagName("iframe")[0].id;var iframe = document.getElementById(iframeId);var iframeDocument = iframe.contentDocument;var editorBody = iframeDocument.getElementById("tinymce");editorBody.innerText;'
+  }, 
+  // 위의 코드가 실행 된 결과를 추출
+  function(result) {
+    var editorInnerText = result[0]
+    var length = editorInnerText.length
+    // id 값이 result 인 태그에 결과를 추가한다.
+    document.querySelector('#result').innerText = '현재 글자수 : ' + length
+  }
+)
